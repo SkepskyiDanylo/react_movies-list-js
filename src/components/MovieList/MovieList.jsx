5cb3e5a32ae6c1649ecc800ea9632fe246e2961e
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-06-10 22:03:23 +0200

    fix

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,10 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
-const MovieList = ({ movies }) => (
+export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
   </div>
 );
-
-export default MovieList;
